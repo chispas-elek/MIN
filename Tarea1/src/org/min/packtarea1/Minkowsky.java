4e13c44234a835ffcaf162deea062ce4245c1ab3
chispas-elek <elektro108@gmail.com>
2013-10-08 18:56:45 +0200

    Bases de la clase Minkowsky

--- Tarea1/src/org/min/packtarea1/Minkowsky.java
+++ Tarea1/src/org/min/packtarea1/Minkowsky.java
@@ -1,7 +1,17 @@
 package org.min.packtarea1;
 
+import java.util.Vector;
+
 import weka.core.Instances;
 
 public class Minkowsky {
 
+	private int k,n,m;
+	private Vector ldistances;
+	
+	private Minkowsky(int pK,int pN,int pM) {
+		this.k = pK;
+		this.n = pN;
+		this.m = pM;
+	}
 }
