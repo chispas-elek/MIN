7acc5efb15f123c1cdc62d7f50e03b161ed13dc4
samvrai <t3echelon@gmail.com>
2013-12-09 16:56:55 +0100

    KMeans iniciado

--- MinFinal/src/org/min/packkmean/KMeans.java
+++ MinFinal/src/org/min/packkmean/KMeans.java
@@ -1,5 +1,10 @@
 package org.min.packkmean;
 
 public class KMeans {
+	//Calcula la pertenencia y recalcula el centroide de cada cluster
 	
+	
+	public KMeans(ListaEntidades pListaEntidades) {
+		
+	}
 }
